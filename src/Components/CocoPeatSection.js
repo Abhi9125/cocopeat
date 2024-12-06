5f63964c948a5415303a260674aa2bab5c9f9e4f
Abhi9125 <abhi.singh3132@gmail.com>
2024-12-06 17:41:47 +0530

    CocoPeat Section update

--- src/Components/CocoPeatSection.js
+++ src/Components/CocoPeatSection.js
@@ -4,8 +4,8 @@ import CocoPeatBenefits from "./CocoPeatBenefits";
 
 const CocoPeatSection = () => {
   return (
-    <section className="py-16 px-6 bg-[#F5F1E0]">
-      <div className="container mx-auto space-y-16">
+    <section className="py-8 sm:py-12 px-4 sm:px-6 bg-[#F5F1E0]">
+      <div className="container mx-auto space-y-12">
         <GrowingCrops />
         <CocoPeatBenefits />
       </div>
